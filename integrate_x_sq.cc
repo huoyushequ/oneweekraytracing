b794a72ca0cb1425a01474c5331516bbd63c7dd9
QIAOQIAN <huoyushequ@sina.com>
2022-05-13 14:29:16 +0800

    one week ray tracing

---
+++ integrate_x_sq.cc
@@ -0,0 +1,19 @@
+#include "rtweekend.h"
+#include <iomanip>
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+inline double pdf(double x) { return 0.5 * x; }
+
+int main() {
+  int N = 1000000;
+  auto sum = 0.0;
+  for (int i = 0; i < N; i++) {
+    auto x = sqrt(random_double(0, 4));
+    sum += x * x / pdf(x);
+  }
+
+  std::cout << std::fixed << std::setprecision(12);
+  std::cout << "I = " << sum / N << '\n';
+}
